5d36359ba75c2fad47bec45a64f3090e326b23ce
horatiu84 <horatiul@yahoo.com>
2024-08-05 15:33:02 +0300

    started a basic project

---
+++ miniProject/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// function printDouble(msg: string): void {
+//   console.log(msg);
+//   console.log(msg);
+// }
+// printDouble("Hello from TypeScript!");
+const btn = document.getElementById("btn");
+console.log(btn);
---
+++ miniProject/src/index.ts
@@ -0,0 +1,9 @@
+// function printDouble(msg: string): void {
+//   console.log(msg);
+//   console.log(msg);
+// }
+
+// printDouble("Hello from TypeScript!");
+
+const btn = document.getElementById("btn");
+console.log(btn);
